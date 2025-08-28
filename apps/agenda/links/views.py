31034f654826a2224a58fe9039332ef875d4c578
Eduardo Antonio Martinez <eduardoa4848@Outlook.es>
2025-08-27 21:46:35 -0500

    ya se pueden agregar eventos y tipos de eventos al erp

--- apps/agenda/links/views.py
+++ apps/agenda/links/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import json
 from django.http import JsonResponse
-from ..models import TypeAppoint
+from ..models import TypeAppoint, Appointment
 from django.db.models import F
 from datetime import datetime
 import sys
@@ -14,18 +14,7 @@ def agenda_home(request):
 
 #-----------------------------EVENTS-------------------------------------
 def create_event(request):
-    if request.method == 'POST':
-        try:
-            body_json = json.loads(request.body)  # convert bytes to Python
-        except Exception as e:
-            return JsonResponse({'success': False, 'message': 'message.error.the-form-have-a-error', 'error': str(e)}, status=400)
-
-        #--------------------
-        #print(body_json)
-        titleEvent = body_json.get("name_event", "").strip()
-        description = body_json.get("description", "")
-
-        #-----------------------------------------------------
+    def get_date_of_the_event(user, body_json: json) -> dict:
         #data that get of the user from the frontend
         date_startDay =  body_json.get("start_date", "")
         date_finishDay =  body_json.get("end_date", "")
@@ -35,18 +24,43 @@ def create_event(request):
         #convert to datetime 
         date_start = datetime.strptime(f"{date_startDay} {date_startHour}", "%Y-%m-%d %H:%M")
         date_finish = datetime.strptime(f"{date_finishDay} {date_finishHour}", "%Y-%m-%d %H:%M")
+        
+        #get the time zone of the user 
+        timezone=user.timezone
+        date_start_utc = Plus.convert_to_utc(date_start, timezone)
+        date_finish_utc = Plus.convert_to_utc(date_finish, timezone)
 
-        date_start_utc = Plus.convert_to_utc(date_start, "America/Mexico_City")
-        date_finish_utc = Plus.convert_to_utc(date_finish, "America/Mexico_City")
- 
-        print(date_start_utc, date_finish_utc)
-        print('normal')
-        print(Plus.convert_from_utc(date_start_utc, "America/Mexico_City"), Plus.convert_from_utc(date_finish_utc, "America/Mexico_City"))
+        return {
+            'start': Plus.convert_from_utc(date_start_utc, timezone),
+            'end': Plus.convert_from_utc(date_finish_utc, timezone)
+        }
+    
+    def validate_date_range(start: datetime, end: datetime) -> bool:
+        """
+        Verifies that the start date is less or equals than the end date.
+        Returns True if valid, False otherwise.
+        """
+        if start > end:
+            return False
+        return True
 
-        #-----------------------------------------------------
 
-        time_alert = body_json.get("alert_time", "0") #this is for know when to alert in minute
-        priority = body_json.get("priority", "0") #for default the priority is equal to 0
+
+    if request.method == 'POST':
+        try:
+            body_json = json.loads(request.body)  # convert bytes to Python
+        except Exception as e:
+            return JsonResponse({'success': False, 'message': 'message.error.the-form-have-a-error', 'error': str(e)}, status=400)
+
+        #get the data of the user
+        user = request.user
+
+        #--------------------get the data of the form---------------------
+        titleEvent = body_json.get("name_event", "").strip()
+        description = body_json.get("description", "")
+
+        time_alert = body_json.get("alert_time") or 0 #this is for know when to alert in minute
+        priority = body_json.get("priority") or 0 #for default the priority is equal to 0
         activate_event_all_the_day = body_json.get("activate_event_all_the_day") == "on"
 
 
@@ -55,21 +69,57 @@ def create_event(request):
         link = body_json.get("link", "")
 
         repeat_this_event = body_json.get("repeat_this_event") == "on" #True/False
-        time_repeat =  body_json.get("time_repeat", "") #this is for that the ERP knows how often to repeat this event in days
-        finish_repeat_date = body_json.get("time_end_repeat", "") 
+        time_repeat =  body_json.get("time_repeat") or 0 #this is for that the ERP knows how often to repeat this event in days
 
 
         send_notification = body_json.get("send_notification") == "on"  # True/False
         i_am_free = body_json.get("i_am_free") == "on"  # True/False
         
         id_type_appoint = body_json.get("type_event", None) #get the id of the type of event
-        user = request.user
 
-        #her we will see if the user added a name to the event
-        if not titleEvent:
-            return JsonResponse({'success': False, 'message': 'message.error.need-a-name-for-your-event'}, status=400)
         
+        #first here we will see if the user added a name to the event
+        if not titleEvent:
+            return JsonResponse({'success': False, 'message': 'message.error.need-a-name-for-your-event'}, status=200)
 
+        #---------------------------get the information of the date of the event--------------------------
+        event_dates = get_date_of_the_event(user, body_json)
+        date_start=event_dates['start'] #date when start the event
+        date_finish=event_dates['end'] #date when finish the event
+
+        #here we will see if the date start is valid and the date finish is valid
+        if not validate_date_range(date_start, date_finish):
+            return JsonResponse({'success': False, 'message': 'message.error.invalid-date-range'}, status=200)
+        
+        #now we will to create a appointment
+        try:
+            appointment = Appointment.objects.create(
+                title=titleEvent,
+                description=description,
+                date_start=date_start,
+                date_finish=date_finish,
+                time_alert=int(time_alert),
+                priority=int(priority),
+                activate_event_all_the_day=activate_event_all_the_day,
+                emails_guests=emails_guests,
+                location=location,
+                link=link,
+                repeat_this_event=repeat_this_event,
+                time_repeat=int(time_repeat) if time_repeat else 0,
+                finish_repeat_date=None,
+                send_notification=send_notification,
+                i_am_free=i_am_free,
+                id_type_appoint_id=id_type_appoint, 
+                user=user
+            )
+
+            #return the new event with his id for that in the frontend can use it
+            return JsonResponse({'success': True, 'message': 'message.success.add-new-event', 'event_id': appointment.id}, status=200)
+
+        except Exception as e:
+            print("Error creating event:", e)
+            return JsonResponse({'success': False, 'message': 'message.success.not-can-add-new-event', 'error': str(e)}, status=200)
+        
 
     return JsonResponse({'success': False, 'message': 'Invalid request method.'}, status=400)
 
